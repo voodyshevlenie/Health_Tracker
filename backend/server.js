b40c2c89d277f7f2a8151eea8634aa09a224a689
voodyshevlenie <minnebaevdamir111@gmail.com>
2024-12-02 23:34:16 +0300

    back working!

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+// server.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const activityRoutes = require('./routes/activityRoutes');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/health-tracker', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    family: 4,
+})
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+app.use('/api/auth', authRoutes);
+app.use('/api/activities', activityRoutes);
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
